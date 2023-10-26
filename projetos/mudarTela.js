be675f539daa12a072ac22f59d6571e002ab022a
heitoryamamoto <132718821+heitoryamamoto@users.noreply.github.com>
2023-10-26 11:08:36 -0300

    Add files via upload

---
+++ projetos/mudarTela.js
@@ -0,0 +1,9 @@
+function perfil(params) {
+    window.location.assign("perfil.html")
+}
+function calculadora(params) {
+    window.location.assign("calculadora.html")
+}
+function icon(params) {
+    window.location.assign("icon.html")
+}
