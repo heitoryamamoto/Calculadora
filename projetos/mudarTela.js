5ec374495b79bc2ad1b1be0b35c8d3719c772b01
heitoryamamoto <132718821+heitoryamamoto@users.noreply.github.com>
2023-10-29 21:29:08 -0300

    mudancas

--- projetos/mudarTela.js
+++ projetos/mudarTela.js
@@ -2,8 +2,26 @@ function perfil(params) {
     window.location.assign("perfil.html")
 }
 function calculadora(params) {
-    window.location.assign("calculadora.html")
+    var usuario = "", senha = ""
+    
+    usuario = document.getElementById("usuario1").value
+    senha = document.getElementById("senha1").value
+    if (usuario == "heitor") {
+        usuario = true;
+    }
+    if (senha == "heitor426345") {
+        senha = true;
+    }
+    if(usuario == true && senha == true) {
+        window.location.assign("calculadora.html")
+    }
+    else{
+        alert("usuário ou senha estão incorretos")
+    }
 }
 function icon(params) {
     window.location.assign("icon.html")
+}
+function menu(params) {
+    window.location.assign("menu.html")
 }
